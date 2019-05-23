6a0a98a54588ab40f3712ff8483270d5752072bf
Mihail Fedotin <mkfedotin@edu.hse.ru>
2019-05-23 12:27:53 +0300

    cubs r rilcool

---
+++ FluidScreen/Assets/CameraScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform viewTarget;
+    public Transform cameraTrans;
+
+    public float scrollSpeed = 5f;
+    public float moveRate = 0.125f;
+
+    public Vector3 cameraTargetPosition;
+
+    void Start()
+    {
+        cameraTargetPosition = cameraTrans.position;
+    }
+    
+    void Update()
+    {
+        cameraTrans.LookAt(viewTarget);
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        cameraTargetPosition += Vector3.up * scroll * scrollSpeed;
+        cameraTrans.position = Vector3.Lerp(cameraTrans.position, cameraTargetPosition, moveRate);
+    }
+}
---
+++ FluidScreen/Assets/imageScreen/CursorShadow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorShadow : MonoBehaviour
+{
+    public static CursorShadow instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    [Range(2.5f, 10f)] public float size = 5f;
+    [HideInInspector] public Vector3 mousePosition;
+    [HideInInspector] public Vector3 mouseDirection;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        mouseDirection = Input.mousePosition - mousePosition;
+        mousePosition = Input.mousePosition;
+    }
+}
